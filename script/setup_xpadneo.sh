d7ac6d8b4f9bbacc54930dc7a8ed8d675f700aff
PDunham113 <patrick.dunham.113@gmail.com>
2023-04-13 22:03:11 -0700

    xpadneo setup

---
+++ script/setup_xpadneo.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+
+readonly XPADNEO_REPO='https://github.com/atar-axis/xpadneo.git'
+readonly XPADNEO_TMP='/tmp/xpadneo'
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+readonly SCRIPT_DIR
+
+
+main() {
+	sudo apt update
+
+	setup_xpadneo
+
+}
+
+setup_xpadneo() {
+	echo 'Checking out latest xpadneo repo...'
+	mkdir -p "${XPADNEO_TMP}"
+	git clone "${XPADNEO_REPO}" "${XPADNEO_TMP}"
+	cd "${XPADNEO_TMP}"
+
+	echo 'Downloading deps...'
+	sudo apt-get install dkms linux-headers-`uname -r`
+
+	echo 'Installing...'
+	sudo ./install.sh
+
+	echo 'Installation complete!'
+}
+
+main "$@"
