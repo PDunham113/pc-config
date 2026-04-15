fe74973c4461db859c65b8e2ad989b6e247d6e61
PDunham113 <patrick.dunham.113@gmail.com>
2026-04-14 17:34:01 -0700

    maintenance + home server rearrangement

--- script/setup_alacritty.sh
+++ script/setup_alacritty.sh
@@ -58,7 +58,7 @@ setup_alacritty() {
 
 	# Move our config file
 	mkdir -p "${HOME}/.config/alacritty"
-	cp "${SCRIPT_DIR}/../config/alacritty:alacritty.yml" "${HOME}/.config/alacritty/alacritty.yml"
+	cp "${SCRIPT_DIR}/../config/alacritty:alacritty.toml" "${HOME}/.config/alacritty/alacritty.toml"
 
 	echo 'Installation complete!'
 }
